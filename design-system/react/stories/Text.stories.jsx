2b4c9e8e543d46106a68485b3e564d916e21d3d9
Marco <m.stefani@itsrizzoli.it>
2022-06-14 12:18:13 +0200

    feat: commit

---
+++ design-system/react/stories/Text.stories.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Text } from '../src/atoms/text/Text';
+
+export default {
+    title: 'Example/atoms/Text',
+    component: Text,
+
+    argTypes:{
+
+    },
+};
+const Template = (args) => <Text {...args}/>;
+
+export const PlainText = Template.bind({});
+
+PlainText.args = {
+
+};
