2f93092c12a7b4ed9411c8ad66339dca450afbcd
Marco <m.stefani@itsrizzoli.it>
2022-06-22 15:40:42 +0200

    feat: file changes

---
+++ modern-css-with-tailwind/src/Fontawesome.js
@@ -0,0 +1,4 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { solid, regular, brands } from '@fortawesome/fontawesome-svg-core/import.macro'
+
+<FontAwesomeIcon icon={solid('apple')} />
